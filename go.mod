274e89ab9214f2e1ea156eade5d012c1a83eb02e
vlad <vlad@dcatk.pro>
2025-09-10 00:01:07 +0300

    test: add tests for buraksezer consistent hashing library
    - Add new test function TestBuraksezerConsistent to benchmark and validate the buraksezer/consistent library
    - Update existing tests to use random keys for more realistic testing
    - Add dependencies for xxhash and buraksezer/consistent in go.mod
    - Include load balancing checks and removal tests for robustness
    
    BREAKING CHANGE: Updated test structure may affect CI if tests are run in specific environments, but no API changes.

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/nullpointerfan/consistent-hashing-tests
 
 go 1.24.3
 
-require github.com/ArchishmanSengupta/consistent-hashing v1.0.2
+require (
+	github.com/ArchishmanSengupta/consistent-hashing v1.0.2
+	github.com/cespare/xxhash v1.1.0
+)
+
+require github.com/buraksezer/consistent v0.10.0
--- main_test.go
+++ main_test.go
@@ -5,34 +5,54 @@ import (
 	"fmt"
 	"hash/fnv"
 	"log"
+	"math/rand"
 	"strconv"
 	"testing"
+	"time"
 
 	consistent_hashing "github.com/ArchishmanSengupta/consistent-hashing"
+	"github.com/buraksezer/consistent"
+	"github.com/cespare/xxhash"
 )
 
+var (
+	hosts = []string{"host 1", "host 2", "host 3", "host 4", "host 5", "host 6", "host 7", "host 8"}
+)
+
+func generateRandomString(length int) string {
+	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	seededRand := rand.New(rand.NewSource(time.Now().UnixNano()))
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = charset[seededRand.Intn(len(charset))]
+	}
+	return string(b)
+}
+
 func TestConsistentHash(t *testing.T) {
+	keys := make([]string, 0, 10000)
+	for range 10000 {
+		keys = append(keys, generateRandomString(64))
+	}
+
 	ch := NewConsistentHash(3, nil)
 
 	if ch.Get("123") != "" {
 		t.Error()
 	}
 
-	hosts := []string{"host 1", "host 2", "host 3", "host 4"}
 	for _, host := range hosts {
 		ch.Add(host)
 	}
 
 	hostMap := make(map[string]string, 0)
-	for i := range 10000 {
-		key := fmt.Sprintf("key%d", i)
+
+	for _, key := range keys {
 		host := ch.Get(key)
-		t.Log(host, i)
 		hostMap[key] = host
 	}
 
-	for i := range 10000 {
-		key := fmt.Sprintf("key%d", i)
+	for _, key := range keys {
 		host := ch.Get(key)
 		if host != hostMap[key] {
 			t.Errorf("%v != %v", host, hostMap[key])
@@ -52,13 +72,18 @@ func TestConsistentHash(t *testing.T) {
 func TestArchishmanSengConsistentHash(t *testing.T) {
 	ch, err := consistent_hashing.NewWithConfig(consistent_hashing.Config{
 		ReplicationFactor: 3,
+		LoadFactor:        1.25,
 	})
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	keys := make([]string, 0, 10000)
+	for range 10000 {
+		keys = append(keys, generateRandomString(64))
+	}
+
 	// Add hosts to the consistent hash ring
-	hosts := []string{"host 1", "host 2", "host 3", "host 4"}
 	ctx := context.TODO()
 	for _, host := range hosts {
 		err := ch.Add(ctx, host)
@@ -68,26 +93,13 @@ func TestArchishmanSengConsistentHash(t *testing.T) {
 	}
 
 	hostMap := make(map[string]string, 0)
-	for i := range 10000 {
-		key := fmt.Sprintf("key%d", i)
+	for _, key := range keys {
 		host, err := ch.GetLeast(ctx, key)
 		if err != nil {
 			t.Error(err)
 		}
 		ch.IncreaseLoad(ctx, host)
-		t.Log(host, i)
 		hostMap[key] = host
-	}
-
-	for i := range 10000 {
-		key := fmt.Sprintf("key%d", i)
-		host, err := ch.Get(ctx, key)
-		if err != nil {
-			t.Error(err)
-		}
-		if host != hostMap[key] {
-			t.Errorf("%v != %v", host, hostMap[key])
-		}
 	}
 
 	loads := ch.GetLoads()
@@ -97,10 +109,96 @@ func TestArchishmanSengConsistentHash(t *testing.T) {
 	}
 }
 
+type myMember string
+
+func (m myMember) String() string {
+	return string(m)
+}
+
+type hasher struct{}
+
+func (h hasher) Sum64(data []byte) uint64 {
+	// you should use a proper hash function for uniformity.
+	return xxhash.Sum64(data)
+}
+
+func TestBuraksezerConsistent(t *testing.T) {
+	keys := make([]string, 0, 10000)
+	for range 10000 {
+		keys = append(keys, generateRandomString(64))
+	}
+
+	// Create a new consistent instance
+	cfg := consistent.Config{
+		PartitionCount:    103,
+		ReplicationFactor: 5,
+		Load:              1.25,
+		Hasher:            hasher{},
+	}
+	c := consistent.New(nil, cfg)
+
+	for _, host := range hosts {
+		c.Add(myMember(host))
+	}
+
+	hostMap := make(map[string]string, 0)
+
+	for _, key := range keys {
+		host := c.LocateKey([]byte(key))
+		hostMap[key] = host.String()
+	}
+
+	for _, key := range keys {
+		host := c.LocateKey([]byte(key))
+		if host.String() != hostMap[key] {
+			t.Errorf("%v != %v", host, hostMap[key])
+		}
+	}
+
+	loadMap := make(map[string]int, 4)
+	for _, host := range hostMap {
+		loadMap[host] += 1
+	}
+
+	sum := 0
+	for key, value := range loadMap {
+		t.Logf("Host: %v, Load: %v", key, value)
+		sum += value
+	}
+	t.Logf("Sum: %v", sum)
+
+	c.Remove(hosts[2])
+	c.Remove(hosts[5])
+
+	for _, key := range keys {
+		host := c.LocateKey([]byte(key))
+		hostMap[key] = host.String()
+	}
+
+	for _, key := range keys {
+		host := c.LocateKey([]byte(key))
+		if host.String() != hostMap[key] {
+			t.Errorf("%v != %v", host, hostMap[key])
+		}
+	}
+
+	loadMap = make(map[string]int, 4)
+	for _, host := range hostMap {
+		loadMap[host] += 1
+	}
+
+	sum = 0
+	t.Log("After remove:")
+	for key, value := range loadMap {
+		t.Logf("Host: %v, Load: %v", key, value)
+		sum += value
+	}
+	t.Logf("Sum: %v", sum)
+}
+
 func BenchmarkConsistentHash(b *testing.B) {
 	ch := NewConsistentHash(3, nil)
 
-	hosts := []string{"host 1", "host 2", "host 3", "host 4"}
 	for _, host := range hosts {
 		ch.Add(host)
 	}
@@ -122,7 +220,6 @@ func BenchmarkArchishmanSengConsistentHash(b *testing.B) {
 	}
 
 	// Add hosts to the consistent hash ring
-	hosts := []string{"host 1", "host 2", "host 3", "host 4"}
 	ctx := context.Background()
 	for _, host := range hosts {
 		err := ch.Add(ctx, host)
